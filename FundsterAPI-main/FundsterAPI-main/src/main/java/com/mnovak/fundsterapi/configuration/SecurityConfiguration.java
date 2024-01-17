cb65586b4afe11e58d9a7258233746090809177c
Ivek <91628099+Poorple@users.noreply.github.com>
2024-01-17 18:31:50 +0100

    Add files via upload

---
+++ FundsterAPI-main/FundsterAPI-main/src/main/java/com/mnovak/fundsterapi/configuration/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.mnovak.fundsterapi.configuration;
+
+import com.mnovak.fundsterapi.entity.User;
+import com.mnovak.fundsterapi.enumeration.Role;
+import com.mnovak.fundsterapi.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorization ->
+                        authorization
+                                .requestMatchers(HttpMethod.POST,"/api/users/**").permitAll()
+                                .requestMatchers(HttpMethod.DELETE,"/api/users/**").hasAnyAuthority(Role.ADMIN.name())
+                                .requestMatchers("/api/users/**").hasAnyAuthority(Role.USER.name(),Role.ADMIN.name())
+                                .requestMatchers(HttpMethod.DELETE, "/api/projects/**").hasAnyAuthority(Role.ADMIN.name())
+                                .requestMatchers("/api/projects/**").hasAnyAuthority(Role.USER.name(),Role.ADMIN.name())
+                                .anyRequest().authenticated()
+                        )
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
+    public FilterRegistrationBean<?> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<?> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
+    }
+}
